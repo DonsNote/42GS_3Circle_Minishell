6273f6e0b88eae4ff6fae7048afaef3c91ace1a9
Don <neveradio@gmail.com>
2024-11-12 17:12:24 +0900

    feat_checkparam

--- parse/check_param.c
+++ parse/check_param.c
@@ -6,7 +6,7 @@
 /*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 16:29:17 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/11/12 16:53:13 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/11/12 17:11:53 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,29 @@
 
 int	check_param(char *param)
 {
-	return (1);
+	if (check_quote(param))
+		return (1);
+	return (0);
+}
+
+int	check_quote(char *param)
+{
+	int	i;
+	int	q;
+	int	dq;
+
+	i = 0;
+	q = 0;
+	dq = 0;
+	while (param[i] != NULL)
+	{
+		if (param[i] == '\'')
+			q = q + 1;
+		if (param[i] == '\"')
+			dq = dq + 1;
+		++i;
+	}
+	if (q % 2 != 0 || dq % 2 != 0)
+		return (1);
+	return (0);
 }
