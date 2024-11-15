32518f7cf7add9b3b31f72d16d1dce19a7e3bac9
Don <neveradio@gmail.com>
2024-11-15 23:00:44 +0900

    ch_header_add_struct

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 18:22:36 by junseyun          #+#    #+#             */
-/*   Updated: 2024/11/12 17:42:00 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/11/15 22:59:25 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,31 @@ typedef struct s_env_node
 	char				*env_data;
 	struct s_env_node	*next;
 }	t_env_node;
+
+typedef enum token_type
+{
+	TOKEN_TYPE_CMD,
+	TOKEN_TYPE_OP,
+	TOKEN_TYPE_PIPE,
+	TOKEN_TYPE_PARAM,
+}t_token_type;
+
+typedef struct s_type
+{
+	int	cmd;
+	int	option;
+	int	pipe;
+	int	param;
+}	t_type;
+
+typedef struct s_token
+{
+	t_type	type;
+	t_token_type token_type;
+	char	*token;
+}	t_token;
+
+t_token.token_type = TOKEN_TYPE_PIPE;
 
 //parse
 int		check_param(char *param);
