d53b53b48e92490b5c9eba52f360b8b2edf4b0ab
Don <neveradio@gmail.com>
2024-12-20 18:46:42 +0900

    hot_fix

--- Utilities/free.c
+++ Utilities/free.c
@@ -6,7 +6,7 @@
 /*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 22:18:24 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/12/20 08:47:46 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/12/20 11:00:50 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 void	free_token(t_token *token);
 void	free_info(t_info *info);
+void	free_env_token(t_env_token *token);
 
 void	free_all(t_token *token, t_info *info)
 {
-	token = NULL;
+	free_token(token);
+	free_info(info);
 	return ;
 }
 
@@ -43,7 +45,36 @@ void	free_token(t_token *token)
 
 void	free_info(t_info *info)
 {
+	free_env_token(info->env);
+	free_env_token(info->exp);
+	free(info->home);
+	free(info->pwd);
+	free(info->oldpwd);
+	free(info);
+	return ;
+}
 
+void	free_env_token(t_env_token *token)
+{
+	t_env_token	*tmp;
+	t_env_token	*temp;
+
+	tmp = token;
+	temp = NULL;
+	while (tmp != NULL)
+	{
+		if (temp != NULL)
+			free(temp);
+		if (tmp->env_data != NULL)
+			free(tmp->env_data);
+		if (tmp->env_key != NULL)
+			free(tmp->env_key);
+		if (tmp->env_value != NULL)
+			free(tmp->env_value);
+		temp = tmp;
+		tmp = tmp->next;
+	}
+	return ;
 }
 
 void	free_strarray(char **str)
--- minishell_parse.c
+++ minishell_parse.c
@@ -6,7 +6,7 @@
 /*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:42:18 by junseyun          #+#    #+#             */
-/*   Updated: 2024/12/20 00:23:47 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/12/20 11:24:25 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ int	input(char **envp)
 			token = tokenize(param, info);
 			if (token == NULL)
 				return (1);
-			// if (builtin(token))
+			// if (builtin(token, info))
 			// 	return (1);
 		}
 		else
