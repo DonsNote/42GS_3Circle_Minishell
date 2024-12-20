b8d164166d2219759f191b26d8d6bb26a2227d5e
DonsNote <neveradio@gmail.com>
2024-12-20 09:22:33 +0900

    toclar

--- Utilities/free.c
+++ Utilities/free.c
@@ -6,11 +6,14 @@
 /*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 22:18:24 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/12/19 23:03:11 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/12/20 08:47:46 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void	free_token(t_token *token);
+void	free_info(t_info *info);
 
 void	free_all(t_token *token, t_info *info)
 {
@@ -18,10 +21,29 @@ void	free_all(t_token *token, t_info *info)
 	return ;
 }
 
-void	free_str(char *str)
+void	free_token(t_token *token)
 {
-	free(str);
+	t_token	*tmp;
+	t_token	*temp;
+
+	tmp = token;
+	temp = NULL;
+	while (tmp != NULL)
+	{
+		if (temp != NULL)
+			free(temp);
+		free(tmp->data);
+		if (tmp->type == E_TYPE_FILE)
+			close(tmp->fd);
+		temp = tmp;
+		tmp = tmp->next;
+	}
 	return ;
+}
+
+void	free_info(t_info *info)
+{
+
 }
 
 void	free_strarray(char **str)
