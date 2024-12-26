9d1b0a0f95e24d040eff1afa83ecf363d4838404
Don <neveradio@gmail.com>
2024-12-26 19:09:47 +0900

    here_doc

--- parse/here_doc.c
+++ parse/here_doc.c
@@ -6,55 +6,62 @@
 /*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 12:42:14 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/12/26 17:34:01 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/12/26 18:32:41 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int		eof_check(t_token *token, char *eof, char *param, char *file_name);
+int		eof_check(t_token *token, char *eof, char *param);
 void	pipe_parse(t_token *token, t_info *info, char *file_name);
 char	*make_file_name(char *file_name);
+void	child_process(t_token *token, t_info *info);
 
 void	pipe_parse(t_token *token, t_info *info, char *file_name)
 {
 	pid_t	pid;
-	char	*param;
-	char	*eof;
 
-	eof = ft_strdup(token->data);
 	pid = fork();
 	if (pid == -1)
 		return ;
 	if (pid == 0)
-	{
-		while (1)
-		{
-			param = readline("heredoc>");
-			if (param[0] == '\0')
-				continue ;
-			if (eof_check(token, eof, param, file_name))
-				break ;
-			substitution(token, info, NULL);
-			write(token->fd, token->data, ft_strlen(token->data));
-			write(token->fd, "\n", 1);
-			free(param);
-		}
-		exit(0);
-	}
+		child_process(token, info);
 	else if (pid > 0)
+	{
 		waitpid(pid, NULL, 0);
+		token->data = file_name;
+	}
 	return ;
 }
 
-int	eof_check(t_token *token, char *eof, char *param, char *file_name)
+void	child_process(t_token *token, t_info *info)
+{
+	char	*param;
+	char	*eof;
+
+	eof = ft_strdup(token->data);
+	while (1)
+	{
+		param = readline("heredoc>");
+		if (param[0] == '\0')
+			continue ;
+		if (eof_check(token, eof, param))
+			break ;
+		substitution(token, info, NULL);
+		write(token->fd, token->data, ft_strlen(token->data));
+		write(token->fd, "\n", 1);
+		free(param);
+	}
+	exit(0);
+}
+
+int	eof_check(t_token *token, char *eof, char *param)
 {
 	if (ft_strcmp(eof, param) == 0)
 	{
 		free(eof);
 		free(param);
 		free(token->data);
-		token->data = file_name;
 		return (1);
 	}
 	free(token->data);
