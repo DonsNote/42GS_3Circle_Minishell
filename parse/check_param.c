aa0bc652388cd7d06fc7d5c8098a07c596bd93cd
Don <neveradio@gmail.com>
2024-12-29 18:58:10 +0900

    1

--- parse/check_param.c
+++ parse/check_param.c
@@ -6,7 +6,7 @@
 /*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 16:29:17 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/12/29 18:53:40 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/12/29 18:57:42 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,13 +73,13 @@ int	check_redirect(char *param, int *i)
 	if (param[*i] == '>')
 	{
 		jump_sp(param, i);
-		if (param[*i] == '\0' || param[*i + 1] == '|')
+		if (param[*i + 1] == '\0' || param[*i] == '|')
 			return (1);
 	}
 	if (param[*i] == '<')
 	{
 		jump_sp(param, i);
-		if (param[*i] == '\0' || param[*i + 1] == '|')
+		if (param[*i + 1] == '\0' || param[*i] == '|')
 			return (1);
 	}
 	return (0);
