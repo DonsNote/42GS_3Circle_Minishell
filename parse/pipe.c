1fa907a0233a6c5f6d465f0447445904d90299e6
Don <neveradio@gmail.com>
2024-12-29 14:31:06 +0900

    fix_pipe

--- parse/pipe.c
+++ parse/pipe.c
@@ -6,7 +6,7 @@
 /*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 04:48:52 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/12/23 00:36:26 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/12/29 14:30:51 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int	is_pipe(t_token *token)
 {
+	int		i;
 	char	*tmp;
 	t_token	*next;
 
@@ -22,7 +23,10 @@ int	is_pipe(t_token *token)
 		return (1);
 	tmp[0] = '|';
 	tmp[1] = '\0';
-	next = make_new_token(token, 1);
+	i = 1;
+	while (token->data[i] == 32 && token->data[i] != '\0')
+		i++;
+	next = make_new_token(token, i);
 	token->data = tmp;
 	token->next = next;
 	return (0);
